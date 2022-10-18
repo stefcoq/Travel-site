9ab6e3532829a9253a0d290ba076019fba93f601
stephane coqueret <stephane.coqueret@gmail.com>
2022-10-18 14:29:57 +0200

    init local repo

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="nav">
+    <AppLink id="logo" to="/">Travel</AppLink>
+    <AppLink
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="{name: 'destination.show', params:{id: destination.id, slug: destination.slug}}"
+      >
+        {{ destination.name }}
+      </AppLink>
+      <AppLink :to="{name: 'protected'}">Dashboard</AppLink>
+      <AppLink to="https://vueschool.io">Vue School</AppLink>
+
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+import AppLink from './AppLink.vue';
+
+export default {
+    data() {
+        return {
+            destinations: sourceData.destinations
+        };
+    },
+    components: { AppLink }
+}
+</script>
